5c829e0dee225f414001d266b8f874734ecd7c9b
weak-chicken <19397726+Weak-Chicken@users.noreply.github.com>
2020-03-08 00:01:07 +0800

    mock api bug fixed

--- Server/server/express/routes/apis/mock/User.js
+++ Server/server/express/routes/apis/mock/User.js
@@ -31,45 +31,37 @@ router.post('/info', function(req, res, next) {
       req.body.userUUID,
     ];
 
-  if ("0a3eed30-9ad5-11e9-9835-b8ee6591991d".length === userUUID.length) {
-    res.send({
-      "success": true,
-      "flag": flagCode.INFO_UNIVERSAL_SUCCEEDED,
-      "userData": {
-        "userMoney": 233.33,
-        "userInventory": [
-            {
-                "itemID": "313:85",
-                "itemName": "item A",
-                "itemLevel": 1,
-                "ExchangingStatus": false,
-                "ExchangedTime": "",
-            },
-            {
-              "itemID": "137",
-              "itemName": "item B",
-              "itemLevel": 3,
-              "ExchangingStatus": true,
-              "ExchangedTime": "2018-12-12T12:19:13.603Z",
-            },
-            {
-              "itemID": "34532:233",
-              "itemName": "item C",
-              "itemLevel": 5,
+  res.send({
+    "success": true,
+    "flag": flagCode.INFO_UNIVERSAL_SUCCEEDED,
+    "userData": {
+      "userMoney": 233.33,
+      "userInventory": [
+          {
+              "itemID": "313:85",
+              "itemName": "item A",
+              "itemLevel": 1,
               "ExchangingStatus": false,
               "ExchangedTime": "",
-            },
-          ],
-        "userAvatar": "http://134.209.104.6:3000/mock/User/avatar/test.png"
-      }
-    });
-  } else {
-    res.status(400);
-    res.send({
-      "success": false,
-      "flag": flagCode.ERROR_UNIVERSAL_FAILED,
-    });
-  }
+          },
+          {
+            "itemID": "137",
+            "itemName": "item B",
+            "itemLevel": 3,
+            "ExchangingStatus": true,
+            "ExchangedTime": "2018-12-12T12:19:13.603Z",
+          },
+          {
+            "itemID": "34532:233",
+            "itemName": "item C",
+            "itemLevel": 5,
+            "ExchangingStatus": false,
+            "ExchangedTime": "",
+          },
+        ],
+      "userAvatar": "http://134.209.104.6:3000/mock/User/avatar/test.png"
+    }
+  });
 });
 
 router.get('/signout', function(req, res, next) {
